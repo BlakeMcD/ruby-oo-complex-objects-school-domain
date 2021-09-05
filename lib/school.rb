e676e8f29561add46f986e79198c0935f7bb1e8b
BlakeMcD <blakemcd@gmail.com>
2021-09-05 17:16:32 +1000

    assignment passed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School 
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+
+    def add_student(name, grade)
+        if roster[grade] != nil #it already exists     
+            roster[grade] << name
+        else #it doesn't exist
+            roster[grade] = []
+            roster[grade] << name
+        end
+
+
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        sorted = {}
+        roster.each do |grade, students|
+            sorted[grade] = students.sort
+        end
+        sorted
+    end
+end
